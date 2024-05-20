e458179fa35a42e68edfb3d44e8be5e32d3cfa6c
sarahcruzz <sarah.cruz0492@gmail.com>
2024-05-20 16:48:12 -0300

    em andamento

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './global.css'
+import { BrowserRouter} from 'react-router-dom'
+import { Rotas } from './rotas/Rotas'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Rotas />
+    </BrowserRouter>
+  )
+}
+
